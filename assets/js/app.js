8b176e9a98a6bf521f00ee0fa4df347489f004e3
Cortes Resendiz <m.cortes@accenture.com>
2023-09-05 08:36:52 -0600

    subir repo

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navList = document.querySelector('.nav-list');
+
+menuToggle.addEventListener('click', () => {
+  navList.classList.toggle('active');
+  menuToggle.classList.toggle('active');
+});
